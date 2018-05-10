6de0a6b83ac473ca022f884eec517424e9d9fbcc
Mark Sommers <kacysommers@gmail.com>
2018-05-10 16:53:27 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
+	},
+	{	
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-			alert('You found a match!');
-		}
+		alert('You found a match!');
+	}
 	else {
 		alert('Sorry, try again.');
 	}
@@ -12,9 +33,11 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-	console.log('User flipped ' + cards[cardId]);
+	console.log('User flipped ' + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -23,3 +46,4 @@ var flipCard = function(cardId){
 
 flipCard(0);
 flipCard(2);
+
